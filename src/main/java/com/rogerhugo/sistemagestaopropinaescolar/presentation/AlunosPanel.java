2355d4833f9ff173fc8e766c737eb736b2e28055
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-08-21 18:32:55 +0100

    Adiciona tabela de alunos

--- src/main/java/com/rogerhugo/sistemagestaopropinaescolar/presentation/AlunosPanel.java
+++ src/main/java/com/rogerhugo/sistemagestaopropinaescolar/presentation/AlunosPanel.java
@@ -5,7 +5,10 @@ import com.rogerhugo.sistemagestaopropinaescolar.service.AlunoService;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlunosPanel extends JPanel {
     private JPanel panelMain;
@@ -16,7 +19,9 @@ public class AlunosPanel extends JPanel {
     private JButton buttonRemoverPessoa;
 
     private JPanel panelCenter;
-    private JList<String> listPessoas;
+    private JList<String> listAlunos;
+    private JTable tableAlunos;
+    //private DefaultTableModel model;
     
     private JFrame main;
 
@@ -27,6 +32,7 @@ public class AlunosPanel extends JPanel {
         
         initComponents();
         addListeners();
+        carregarListaAlunos();
     }
 
     private void initComponents() {
@@ -66,11 +72,17 @@ public class AlunosPanel extends JPanel {
         panelCenter = new JPanel(new BorderLayout());
         panelCenter.setBorder(new EmptyBorder(30, 0, 0, 0));
 
-        listPessoas = new JList<>();
-        listPessoas.setBorder(new EmptyBorder(5, 7, 7, 5));
-        listPessoas.setPreferredSize(listPessoas.getPreferredSize());
+        listAlunos = new JList<>();
+        listAlunos.setBorder(new EmptyBorder(5, 7, 7, 5));
+        listAlunos.setPreferredSize(listAlunos.getPreferredSize());
 
-        panelCenter.add(listPessoas);
+        // tableAlunos
+         //model = new DefaultTableModel();
+        tableAlunos = new JTable(/*model*/);
+        JScrollPane scroll = new JScrollPane(tableAlunos);
+
+        //panelCenter.add(listAlunos);
+        panelCenter.add(scroll);
 
         panelMain.add(panelCenter);
     }
@@ -83,5 +95,33 @@ public class AlunosPanel extends JPanel {
             AlunoFormDialog ald = new AlunoEditDialog(main, aluno);
             ald.setVisible(true);
         });
+    }
+
+    private void carregarListaAlunos() {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Nome");
+        model.addColumn("Curso");
+
+        //model.addRow(new Object[]{"Roger", 20, "Informática"});
+        //model.addRow(new Object[]{"Maria", 22, "Direito"});
+
+        List<Aluno> alunos = new ArrayList<>(AlunoService.getInstance().pegarTodos());
+        alunos.forEach(aluno -> model.addRow(new Object[]{aluno.getNome(), aluno.getCurso()}));
+
+        tableAlunos.setModel(model);
+
+        /*List<Aluno> alunos = new ArrayList<>(AlunoService.getInstance().pegarTodos());
+        List<String> nomesAlunos = new ArrayList<>();
+        alunos.forEach(aluno -> nomesAlunos.add(aluno.getNome()));
+
+        int index = listAlunos.getSelectedIndex();
+
+        listAlunos.setListData(nomesAlunos.toArray(new String[0]));
+
+        listAlunos.setSelectedIndex(index);
+
+        System.out.println(alunos);
+        System.out.println(nomesAlunos);
+        System.out.println(nomesAlunos.toArray(new String[0]));*/
     }
 }
